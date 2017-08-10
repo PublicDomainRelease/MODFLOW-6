3e3a48fa8d6158086c7727286b31adeb03aa4fa7
USGS <gs-w_ogw_dev@usgs.gov>
2017-08-11 00:00:00 +1200

    MODFLOW 6 Version 6.0.0 August 11, 2017
    https://water.usgs.gov/ogw/modflow/mf6.0.0.zip

---
+++ examples/runall.bat
@@ -0,0 +1,129 @@
+cd ex01-twri
+..\..\bin\mf6.exe
+cd ..
+
+cd ex02-tidal
+..\..\bin\mf6.exe
+cd ..
+
+cd ex03-bcf2ss
+..\..\bin\mf6.exe
+cd ..
+
+cd ex04-fhb
+..\..\bin\mf6.exe
+cd ..
+
+cd ex05-mfusg1disu
+..\..\bin\mf6.exe
+cd ..
+
+cd ex06-mfusg1disv
+..\..\bin\mf6.exe
+cd ..
+
+cd ex07-mfusg1lgr
+..\..\bin\mf6.exe
+cd ..
+
+cd ex08-mfusg1xt3d
+..\..\bin\mf6.exe
+cd ..
+
+cd ex09-bump
+..\..\bin\mf6.exe
+cd ..
+
+cd ex10-bumpnr
+..\..\bin\mf6.exe
+cd ..
+
+cd ex11-disvmesh
+..\..\bin\mf6.exe
+cd ..
+
+cd ex12-hanicol
+..\..\bin\mf6.exe
+cd ..
+
+cd ex13-hanirow
+..\..\bin\mf6.exe
+cd ..
+
+cd ex14-hanixt3d
+..\..\bin\mf6.exe
+cd ..
+
+cd ex15-whirlsxt3d
+..\..\bin\mf6.exe
+cd ..
+
+cd ex16-mfnwt2
+..\..\bin\mf6.exe
+cd ..
+
+cd ex17-mfnwt3h
+..\..\bin\mf6.exe
+cd ..
+
+cd ex18-mfnwt3l
+..\..\bin\mf6.exe
+cd ..
+
+cd ex19-zaidel
+..\..\bin\mf6.exe
+cd ..
+
+cd ex20-keating
+..\..\bin\mf6.exe
+cd ..
+
+cd ex21-sfr1
+..\..\bin\mf6.exe
+cd ..
+
+cd ex22-lak2
+..\..\bin\mf6.exe
+cd ..
+
+cd ex23-lak4
+..\..\bin\mf6.exe
+cd ..
+
+cd ex24-neville
+..\..\bin\mf6.exe
+cd ..
+
+cd ex25-flowing-maw
+..\..\bin\mf6.exe
+cd ..
+
+cd ex26-Reilly-maw
+..\..\bin\mf6.exe
+cd ..
+
+cd ex27-advpakmvr
+..\..\bin\mf6.exe
+cd ..
+
+cd ex28-mflgr3
+..\..\bin\mf6.exe
+cd ..
+
+cd ex29-vilhelmsen-gc
+..\..\bin\mf6.exe
+cd ..
+
+cd ex30-vilhelmsen-gf
+..\..\bin\mf6.exe
+cd ..
+
+cd ex31-vilhelmsen-lgr
+..\..\bin\mf6.exe
+cd ..
+
+cd ex32-periodicbc
+..\..\bin\mf6.exe
+cd ..
+
+pause
